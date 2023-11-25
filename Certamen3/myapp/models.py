c90941d0a823dec89cf630396093317c5e5e54d9
JuanValdesS <juan.valdess@usm.cl>
2023-11-25 16:40:40 -0300

    Se agregaron los models

--- Certamen3/myapp/models.py
+++ Certamen3/myapp/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
+class Tipo(models.Model):
+    nombre = models.CharField(max_length=50, unique=True)  #Unique = true; indica que el dato es unico en la tabla 
+
+    def __str__(self):
+        return self.nombre
+
+class Segmento(models.Model):
+    nombre = models.CharField(max_length=50, unique=True)
+
+    def __str__(self):
+        return self.nombre
+
+class Evento(models.Model):
+    id = models.AutoField(primary_key=True)
+    fecha_inicio = models.DateTimeField()
+    fecha_termino = models.DateTimeField()
+    titulo = models.CharField(max_length=100)
+    descripcion = models.TextField()
+    tipo = models.ForeignKey(Tipo, on_delete=models.CASCADE)
+    segmento = models.ForeignKey(Segmento, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.titulo
+
 # Create your models here.
