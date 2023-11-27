433b90d1784104ba0b36096ce98a37e30e22c17f
diego-vv <141526344+diego-vv@users.noreply.github.com>
2023-11-27 16:53:41 -0300

    cambie admin

--- Certamen3/myapp/admin.py
+++ Certamen3/myapp/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from .models import Tipo,Segmento,Evento
+admin.user.register(Tipo)
+admin.user.register(Segmento)
+admin.user.register(Evento)
 # Register your models here.
