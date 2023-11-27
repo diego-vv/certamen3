d807a358e5174107db9d24b782f401c2d6b6e82e
diego-vv <141526344+diego-vv@users.noreply.github.com>
2023-11-27 17:10:44 -0300

    ahora si cambios admin

--- Certamen3/myapp/admin.py
+++ Certamen3/myapp/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from .models import Tipo,Segmento,Evento
-admin.user.register(Tipo)
-admin.user.register(Segmento)
-admin.user.register(Evento)
+
+admin.site.register(Tipo)
+admin.site.register(Segmento)
+admin.site.register(Evento)
 # Register your models here.
