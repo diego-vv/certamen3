2c76e16980e81119c8ba910a73d271ed2e8d2969
diego-vv <141526344+diego-vv@users.noreply.github.com>
2023-11-28 01:58:40 -0300

    resolver conflicto admin

--- Certamen3/myapp/admin.py
+++ Certamen3/myapp/admin.py
@@ -1,13 +1,10 @@
 from django.contrib import admin
-<<<<<<< HEAD
 from .models import Tipo, Segmento, UserProfile
-=======
 from .models import Tipo,Segmento,Evento
 
 admin.site.register(Tipo)
 admin.site.register(Segmento)
 admin.site.register(Evento)
->>>>>>> d807a358e5174107db9d24b782f401c2d6b6e82e
 # Register your models here.
 
 
